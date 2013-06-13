250f4a8f3cf76ddd2c3d2f00e7e0e00977d7e3b0
yujiosaka <isobyu01@luther.edu>
2013-06-13 17:43:20 +0900

    changed it for our service

--- _config.js
+++ _config.js
@@ -1,36 +1,20 @@
-var USER_AGENTS = require('./const.js').USER_AGENTS;
-
 // Twilioのアカウント情報を保持するオブジェクト。電話をかける際に使用します。
 exports.twilio = {
   sid: 'ACCOUNT SID',
   token: 'AUTH TOKEN'
 };
 
-// SHOPごとの設定を保持するオブジェクト
-exports.shops = [
+// サーバーごとの設定を保持するオブジェクト
+exports.servers = [
   {
     name: 'デモサイト',
-    cronTime: '*/5 * * * *', // 5分毎にログインをチェック
-    login_url: 'https://example.com/Login', // ログイン処理のURL（≠ログインフォームのURL）
-    form: {
-      userId: 'test01@example.com',
-      password: 'test01'
-    },
-    headers: {
-      'User-Agent': USER_AGENTS.PC.FIRE_FOX_10 // const.jsを参照
-    },
-    canSendMail: true,
-    timeout: 3000 * 10, // タイムアウト設定（ミリ秒）
-    regexp: /\"ログアウト\"/, // ログイン後のページでこの正規表現に一致するテキストが存在する場合にログイン成功と認識されます
-    mail: { // メール送信設定
-      from: 'alert@example.com',
-      to: ['test01@example.com', 'test02@example.com']
-    },
-    canMakeCall: true,
+    cronTime: '* * * * *', // 分毎
+    url: 'https://example.com/Login', // URL
+    timeout: 5000, // タイムアウト設定（ミリ秒）
     call: {
       from: '+81000000000', // Twilioで取得（購入）した電話番号
-      to: '+81000000000',
-      url: "Emergency! Emergency! An login attempt failed at Demo Site. Please check your email for more details. Thanks!" // In English
+      tos: '+81000000000',
+      message: "Emergency! Emergency! An login attempt failed at Demo Site. Please check your email for more details. Thanks!" // In English
     }
   }
 ];
--- app.js
+++ app.js
@@ -1,56 +1,31 @@
-var shops = require('./config.js').shops,
+var servers = require('./config.js').servers,
     lib = require('./lib'),
     request = require('request'),
     cronJob = require('cron').CronJob;
 
-shops.forEach(function(shop) {
-  shop.prevLoginSuccessful = true;
+servers.forEach(function(server) {
+  server.prevLoginSuccessful = true;
   job = new cronJob({
-    cronTime: shop.cronTime,
+    cronTime: server.cronTime,
     onTick: function() {
-      request.post({
-        url: shop.login_url,
-        form: shop.form,
-        headers: { 'User-Agent': shop.userAgent },
-        timeout: shop.timeout,
-        followRedirect: true,
-        followAllRedirects: true
+      request.get({
+        url: server.url,
+        timeout: server.timeout
       }, function (err, res, body) {
-        var loggedin = shop.regexp.exec(body);
-        if (loggedin) {
-          console.log('【' + shop.name + '】ログイン処理に成功しました');
-          if (!shop.prevLoginSuccessful) {
-            var subject = '【' + shop.name + '】ログイン障害から復旧しました';
-            var text = lib.createMailText(err, body, shop, true)
-            lib.sendMail(shop.mail.from, shop.mail.to, subject, text, function(err) {
+        if (err) {
+          if (server.prevLoginSuccessful) {
+            console.log('電話の呼び出し中です。');
+            lib.makeCall(server.call.from, server.call.tos, server.call.message, function(err) {
               if (err) {
-                console.log('メールの送信に失敗しました。詳細は以下のログをご確認ください');
+                console.log('電話の呼び出しに失敗しました。詳細は以下のログをご確認ください');
                 console.log(err);
               }
             });
+            server.prevLoginSuccessful = false;
           }
-          shop.prevLoginSuccessful = true;
         } else {
-          console.log('【' + shop.name + '】ログイン処理に失敗しました');
-          if (shop.prevLoginSuccessful) {
-            var subject = '【' + shop.name + '】ログイン障害が発生しました';
-            var text = lib.createMailText(err, body, shop, false);
-            lib.sendMail(shop.mail.from, shop.mail.to, subject, text, function(err) {
-              if (err) {
-                console.log('メールの送信に失敗しました。詳細は以下のログをご確認ください');
-                console.log(err);
-              }
-            });
-            if (shop.canMakeCall) {
-              lib.makeCall(shop.call.from, shop.call.to, shop.call.message, function(err) {
-                if (err) {
-                  console.log('電話の呼び出しに失敗しました。詳細は以下のログをご確認ください');
-                  console.log(err);
-                }
-              });
-            }
-          }
-          shop.prevLoginSuccessful = false;
+          server.prevLoginSuccessful = true;
+          console.log(body);
         }
       });
     }
