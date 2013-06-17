e825c2b3bd39f51eef0b08b37b659175d0d69712
yujiosaka <isobyu01@luther.edu>
2013-06-17 23:14:55 +0900

    do not use login url for default

--- _config.js
+++ _config.js
@@ -9,7 +9,7 @@ exports.servers = [
   {
     name: 'デモサイト',
     cronTime: '* * * * *', // 分毎
-    url: 'https://example.com/Login', // URL
+    url: 'https://example.com/', // URL
     timeout: 10000, // タイムアウト設定（ミリ秒）
     call: {
       from: '+81000000000', // Twilioで取得（購入）した電話番号
